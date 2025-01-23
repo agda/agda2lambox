00fa4c7e26ed0b358ffa4d32032d940c160451d1
liesnikov <17168103+liesnikov@users.noreply.github.com>
2025-01-23 16:54:04 +0100

    override agda manually

--- flake.nix
+++ flake.nix
@@ -8,7 +8,17 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {inherit system;};
-        agda2lambox = pkgs.haskellPackages.callCabal2nix "agda2lambox" ./. {};
+        hpkgs = with pkgs; haskellPackages.override {
+          overrides = haskell.lib.packageSourceOverrides {
+            Agda = fetchFromGitHub {
+              owner = "agda";
+              repo = "agda";
+              rev = "5c29109f8212ef61b0091d62ef9c8bfdfa16cf36";
+              hash = "sha256-qiV/tk+/b3xYPJcWVVd7x9jrQjBzl1TXHPNEQbKV2rA=";
+            };
+          };
+        };
+        agda2lambox = hpkgs.callCabal2nix "agda2lambox" ./. {};
       in {
         packages = {
           inherit agda2lambox;
