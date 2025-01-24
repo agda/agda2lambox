60ad91a600e191eecc5856555e88b61ac13ef039
liesnikov <17168103+liesnikov@users.noreply.github.com>
2025-01-24 11:43:40 +0100

    format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,46 @@
 {
   description = "agda2lambox";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
-  inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = {self, nixpkgs, flake-utils}:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs { inherit system; };
         agdasrc = pkgs.fetchFromGitHub {
           owner = "agda";
           repo = "agda";
           rev = "5c29109f8212ef61b0091d62ef9c8bfdfa16cf36";
           hash = "sha256-qiV/tk+/b3xYPJcWVVd7x9jrQjBzl1TXHPNEQbKV2rA=";
         };
-        hpkgs = with pkgs; haskellPackages.override {
-          overrides = _: old: rec {
-            Agda = haskell.lib.doJailbreak (haskell.lib.overrideSrc old.Agda {src = agdasrc;});
+        hpkgs =
+          with pkgs;
+          haskellPackages.override {
+            overrides = _: old: rec {
+              Agda = haskell.lib.doJailbreak (
+                haskell.lib.overrideSrc old.Agda {
+                  src = agdasrc;
+                  version = "2.8.0";
+                }
+              );
+            };
           };
-        };
-        agda2lambox = hpkgs.callCabal2nix "agda2lambox" ./. {};
-      in {
+        agda2lambox = hpkgs.callCabal2nix "agda2lambox" ./. { };
+      in
+      {
         packages = {
           inherit agda2lambox;
           default = agda2lambox;
         };
         devShells.default = pkgs.haskellPackages.shellFor {
-          packages = p: [agda2lambox];
+          packages = p: [ agda2lambox ];
           buildInputs = with pkgs.haskellPackages; [
             cabal-install
             cabal2nix
@@ -34,5 +48,6 @@
             pkgs.agda
           ];
         };
-      });
+      }
+    );
 }
